be048fa3a810ec78240014f003922284be9c3e39
James Newman <j.newman26@outlook.com>
2025-03-13 19:52:52 +0000

    Created click model

--- backend/server/src/main/java/com/snareclick/server/model/Click.java
+++ backend/server/src/main/java/com/snareclick/server/model/Click.java
@@ -1,5 +1,28 @@
 package com.snareclick.server.model;
 
+import java.util.Date;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clicks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Click {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt;
+    private String trackingCode;
+    private String ipAddress;
+    private String location;
+    private String userAgent;
 
 }
