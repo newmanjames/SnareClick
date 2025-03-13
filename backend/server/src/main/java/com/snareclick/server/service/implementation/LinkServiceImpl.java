c80763e7c480228d1d795e950bfcfd5e0d2acc1d
James Newman <j.newman26@outlook.com>
2025-03-13 23:00:44 +0000

    Implemented link service

---
+++ backend/server/src/main/java/com/snareclick/server/service/implementation/LinkServiceImpl.java
@@ -0,0 +1,39 @@
+package com.snareclick.server.service.implementation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.snareclick.server.model.Link;
+import com.snareclick.server.repository.LinkRepository;
+import com.snareclick.server.service.LinkService;
+
+@Service
+public class LinkServiceImpl implements LinkService {
+
+    private final LinkRepository linkRepository;
+
+    LinkServiceImpl(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    @Override
+    public Link createLink(String redirectURL) {
+        // create new link and set URL
+        Link link = new Link();
+        link.setRedirectURL(redirectURL);
+
+        // save link to database
+        linkRepository.save(link);
+
+        return link;
+
+    }
+
+    @Override
+    public Optional<Link> findByID(UUID id) {
+        return linkRepository.findById(id);
+    }
+
+}
