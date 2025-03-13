28065fde2fc7ac0ed7fb3a5b509d6d06b917db71
James Newman <j.newman26@outlook.com>
2025-03-13 19:53:05 +0000

    Created Link model

--- backend/server/src/main/java/com/snareclick/server/model/Link.java
+++ backend/server/src/main/java/com/snareclick/server/model/Link.java
@@ -1,5 +1,24 @@
 package com.snareclick.server.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String redirectURL;
+    private String trackingCode;
 
 }
