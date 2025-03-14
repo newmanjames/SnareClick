f5f54cddd4c8216903a43e9824d485869bbe1764
James Newman <j.newman26@outlook.com>
2025-03-14 00:59:14 +0000

    Added findByLink method

--- backend/server/src/main/java/com/snareclick/server/repository/ClickRepository.java
+++ backend/server/src/main/java/com/snareclick/server/repository/ClickRepository.java
@@ -1,9 +1,13 @@
 package com.snareclick.server.repository;
 
 import com.snareclick.server.model.Click;
+import com.snareclick.server.model.Link;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ClickRepository extends JpaRepository<Click, Long> {
+    List<Click> findByLink(Link link);
 }
