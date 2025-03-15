98106b8b0bf7f43b852fcb9db56375274d1c81ef
James Newman <j.newman26@outlook.com>
2025-03-15 01:16:41 +0000

    GET /api/create implemented

--- backend/server/src/main/java/com/snareclick/server/controller/api/CreateController.java
+++ backend/server/src/main/java/com/snareclick/server/controller/api/CreateController.java
@@ -1,16 +1,37 @@
 package com.snareclick.server.controller.api;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import com.snareclick.server.dto.LinkDTO;
+import com.snareclick.server.model.Link;
+import com.snareclick.server.service.ClickService;
+import com.snareclick.server.service.LinkService;
 
 @RestController
 @RequestMapping("/api/create")
 public class CreateController {
 
-    @GetMapping
-    public String getMethodName() {
-        return "Hello";
+    private final LinkService linkService;
+
+    public CreateController(ClickService clickService, LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LinkDTO> createLink(@RequestBody Link link) {
+
+        String redirectURL = link.getRedirectURL();
+
+        Link savedLink = linkService.createLink(redirectURL);
+
+        return ResponseEntity.ok(new LinkDTO(savedLink.getId()));
+
     }
 
 }
