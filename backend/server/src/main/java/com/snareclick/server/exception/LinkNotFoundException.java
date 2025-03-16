a405d205b749e399bf4e776e3931e0f0c5f36685
James Newman <j.newman26@outlook.com>
2025-03-16 02:39:26 +0000

    Included error message in constructor

--- backend/server/src/main/java/com/snareclick/server/exception/LinkNotFoundException.java
+++ backend/server/src/main/java/com/snareclick/server/exception/LinkNotFoundException.java
@@ -1,11 +1,13 @@
 package com.snareclick.server.exception;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class LinkNotFoundException extends RuntimeException {
-    public LinkNotFoundException(String message) {
-        super(message);
+    public LinkNotFoundException(UUID link_id) {
+        super("Link with ID " + link_id + " not found");
     }
 }
